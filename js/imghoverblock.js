ab9d010184d66e5263a084ec5e506622ad470550
Ilya <ilyasullivan01@gmail.com>
2023-08-10 01:13:46 +0100

    Update to homepage

--- js/imghoverblock.js
+++ js/imghoverblock.js
@@ -1,17 +1,54 @@
-jQuery(function ($) {
+function initializeSlideshow(activeSlideshow, slides) {
+    var slideIndex = 0;
 
-    // Cycle plugin
-    $('.porfolioSlides').cycle({
-        fx: 'none',
-        speed: 150,
-        timeout: 5
-    }).cycle("pause");
+    function showSlide(index) {
+        slides.forEach((slide, idx) => {
+            if (idx === index) {
+                slide.style.opacity = 1;
+                slide.style.transform = 'scale(1)'; // Apply zoom-in effect to the active slide
+            } else {
+                slide.style.opacity = 0;
+                slide.style.transform = 'scale(0.9)'; // Apply zoom-out effect to other slides
+            }
+        });
+        slideIndex = index;
+    }
 
-    // Pause &amp; play on hover
-    $('.portfolioImage').hover(function () {
-        $(this).find('.porfolioSlides').addClass('active').cycle('resume');
-    }, function () {
-        $(this).find('.porfolioSlides').removeClass('active').cycle('pause');
+    function nextSlide() {
+        slideIndex = (slideIndex + 1) % slides.length;
+        showSlide(slideIndex);
+    }
+
+    var interval; // Declare the interval variable here
+    var isFirstSlide = true; // Flag to track the first slide
+
+    activeSlideshow.addEventListener('mouseenter', function () {
+        interval = setInterval(function () {
+            nextSlide();
+            if (isFirstSlide) {
+                clearInterval(interval); // Clear the initial interval
+                interval = setInterval(nextSlide, 1000); // Start with 1000ms interval
+                isFirstSlide = false;
+            }
+        }, isFirstSlide ? 100 : 1000); // Set interval based on isFirstSlide
     });
 
+    // Stop slideshow when the mouse leaves the activeSlideshow area
+    activeSlideshow.addEventListener('mouseleave', function () {
+        clearInterval(interval);
+        slides.forEach((slide) => {
+            slide.style.opacity = 0; // Hide all slides when stopping the slideshow
+        });
+        showSlide(0);
+        isFirstSlide = true;
+    });
+
+    showSlide(slideIndex); // Show first slide initially
+}
+
+// Get all slideshow elements and initialize them
+var slideshowElements = document.querySelectorAll('.portfolioImage');
+slideshowElements.forEach((slideshowElement) => {
+    var slides = slideshowElement.querySelectorAll('.portfolioSlides li');
+    initializeSlideshow(slideshowElement, slides);
 });
