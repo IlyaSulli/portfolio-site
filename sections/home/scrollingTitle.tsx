744ac18bd8537136cf32412941fc0f7d229d13fb
Ilya <ilyasullivan01@gmail.com>
2025-06-14 00:00:19 +0100

    Completed design of scrolling title

--- sections/home/scrollingTitle.tsx
+++ sections/home/scrollingTitle.tsx
@@ -1,41 +1,58 @@
-import { motion } from "motion/react";
-
-import { SpinnerIcon } from "@/components/icons";
+"use client";
+import { motion, useAnimationFrame, useMotionValue } from "motion/react";
+import { useRef } from "react";
 
 export default function ScrollingTitle() {
-  const scrollingSection = (
-    <>
-      <span className="text-9xl font-medium text-[color:var(--secondary)] mx-8">
-        Software Developer
-      </span>
-      <motion.div
-        animate={{ rotate: 360 }}
-        className="origin-center"
-        initial={{ rotate: 0 }}
-        transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
-      >
-        <SpinnerIcon className="text-9xl text-primary" />
-      </motion.div>
-    </>
-  );
+  const phrases = [
+    "Software Developer",
+    "App Developer",
+    "UI Designer",
+    "Web Developer",
+    "Frontend Developer",
+    "UX Designer",
+    "Mobile Developer",
+  ];
+  const longPhrases = Array(2).fill(phrases).flat();
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+
+  useAnimationFrame((t, delta) => {
+    const container = containerRef.current;
+
+    if (!container) return;
+
+    const velocity = 0.1; // 100px /sec
+
+    const prev = x.get();
+    const next: number = prev - delta * velocity;
+    const resetAt: number = -container.scrollWidth / 2; // scroll back when 1 loop is out
+
+    x.set(next <= resetAt ? 0 : next);
+  });
 
   return (
     <div className="relative w-full overflow-hidden py-4">
       <motion.div
-        animate={{ x: ["0%", "-100%"] }}
-        className="flex items-center whitespace-nowrap"
-        style={{ willChange: "transform" }}
-        transition={{
-          repeat: Infinity,
-          repeatType: "loop",
-          duration: 20,
-          ease: "linear",
-        }}
+        ref={containerRef}
+        className="flex whitespace-nowrap"
+        style={{ x, willChange: "transform" }}
       >
-        <div className="flex">
-          {scrollingSection}
-          {scrollingSection}
-        </div>
+        {longPhrases.map((phrase, index) => (
+          <div key={index} className="flex items-center mx-8">
+            <span className="text-9xl font-medium text-[color:var(--secondary)]">
+              {phrase}
+            </span>
+            <motion.span
+              animate={{ rotate: 360 }}
+              className="origin-center text-9xl font-medium text-primary flex items-center ml-16"
+              initial={{ rotate: 0 }}
+              transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+            >
+              ✱
+            </motion.span>
+          </div>
+        ))}
       </motion.div>
     </div>
   );
