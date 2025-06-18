7dd8e29b2bed265dff4b5e6b415269c53abfeb29
Ilya <ilyasullivan01@gmail.com>
2025-06-18 17:07:03 +0100

    Create Projects page with all items

---
+++ pages/projects/index.tsx
@@ -0,0 +1,16 @@
+import { title } from "@/components/primitives";
+import DefaultLayout from "@/layouts/default";
+import GridProjects from "@/sections/home/projects";
+
+export default function ProjectsPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center mb-8">
+          <h1 className={title()}>Projects</h1>
+        </div>
+        <GridProjects nItems={30} />
+      </section>
+    </DefaultLayout>
+  );
+}
