4bfdc230805fddf80d9173b5efbdaabfc6542b26
Ilya <ilyasullivan01@gmail.com>
2025-06-18 21:58:45 +0100

    Update to framer v5

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "motion/react";
 import { Image } from "@heroui/react";
 
 import { ArrowLeft, ArrowRight } from "@/components/icons";
