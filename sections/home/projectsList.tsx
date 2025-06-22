b1be710708f8a7845621cef235a78c82e96e7022
Ilya <ilyasullivan01@gmail.com>
2025-06-22 15:49:42 +0100

    Removed blurring of projects list grid

--- sections/home/projectsList.tsx
+++ sections/home/projectsList.tsx
@@ -42,7 +42,6 @@ export default function GridProjects({ nItems }: { nItems: number }) {
               }
             >
               <Image
-                isBlurred
                 className="aspect-square object-cover"
                 src={project.thumbnailImage}
                 width={600}
