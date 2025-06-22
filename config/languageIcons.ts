f60a5ae550a65ec7517540118973275a43c120ba
Ilya <ilyasullivan01@gmail.com>
2025-06-22 15:50:28 +0100

    Added new language icons

--- config/languageIcons.ts
+++ config/languageIcons.ts
@@ -1,8 +1,25 @@
-import { DartIcon, FlutterIcon, MySQLIcon, PHPIcon } from "@/components/icons";
+import {
+  CSSIcon,
+  DartIcon,
+  FlutterIcon,
+  MySQLIcon,
+  PHPIcon,
+  HTMLIcon,
+  JavaScriptIcon,
+  NodeJSIcon,
+  JavaIcon,
+  TypeScriptIcon,
+} from "@/components/icons";
 
 export const languageIcons = {
   Dart: DartIcon,
   Flutter: FlutterIcon,
   MySQL: MySQLIcon,
   PHP: PHPIcon,
+  CSS: CSSIcon,
+  HTML: HTMLIcon,
+  JavaScript: JavaScriptIcon,
+  NodeJS: NodeJSIcon,
+  Java: JavaIcon,
+  TypeScript: TypeScriptIcon,
 };
