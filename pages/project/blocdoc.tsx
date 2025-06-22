1f92ca3e7a3435fa82ad3ccd50f3badd148b9d69
Ilya <ilyasullivan01@gmail.com>
2025-06-22 15:49:00 +0100

    Added BlocDoc Project

---
+++ pages/project/blocdoc.tsx
@@ -0,0 +1,17 @@
+import { Image } from "@heroui/image";
+
+import Project from "@/sections/projects/project";
+
+export default function BlocDoc() {
+  return (
+    <Project url="blocdoc">
+      <>
+        <Image src="/image/projects/blocdoc/blocdochomepage.webp" />
+        <Image src="/image/projects/blocdoc/blocdocdocumentlightdark.webp" />
+        <Image src="/image/projects/blocdoc/blocdoceditingmode.webp" />
+        <Image src="/image/projects/blocdoc/blocdocflashcard.webp" />
+        <Image src="/image/projects/blocdoc/blocdocimport.webp" />
+      </>
+    </Project>
+  );
+}
