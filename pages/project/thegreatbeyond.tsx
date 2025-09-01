c03263c1f25a2420af0dff0875b99d2397766203
Ilya <ilyasullivan01@gmail.com>
2025-09-01 23:33:12 +0100

    Add maps for the great beyond to page

--- pages/project/thegreatbeyond.tsx
+++ pages/project/thegreatbeyond.tsx
@@ -1,9 +1,18 @@
+import { Image } from "@heroui/image";
+
 import Project from "@/sections/projects/project";
 
 export default function TheGreatBeyond() {
   return (
     <Project url="thegreatbeyond">
-      <></>
+      <Image
+        alt="Map of the planet of Novaterra"
+        src="/image/projects/thegreatbeyond/novaterramap.png"
+      />
+      <Image
+        alt="Map of the City of Gear Mark"
+        src="/image/projects/thegreatbeyond/gearmarkmap.png"
+      />
     </Project>
   );
 }
