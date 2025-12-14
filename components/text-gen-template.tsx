3c1aa31122d36434847c23818c39acf35c0f0766
Ilya <ilyasullivan01@gmail.com>
2025-12-14 20:52:43 +0000

    Add compact view support to TextGenTemplate component

--- components/text-gen-template.tsx
+++ components/text-gen-template.tsx
@@ -5,11 +5,26 @@ interface TextGenTemplateProps {
     iconHexColor: string;
     name: string;
     description: string;
+    compact?: boolean;
 }
 
 export default function TextGenTemplate({
-    Icon, iconHexColor, name, description
+    Icon, iconHexColor, name, description, compact = false
 }: TextGenTemplateProps){
+    if (compact || !description) {
+        return (
+            <div className="flex flex-col p-4 align-top">
+                <div>
+                    <div className="inline-flex p-3 rounded-lg" style={{ backgroundColor: iconHexColor }}>
+                        <Icon size={24} color="white"/>
+                    </div>
+                </div>
+                
+                <span className="font-semibold text-lg pt-4 line-clamp-2">{name}</span>
+            </div>
+        );
+    }
+
     return (
         <div className="flex flex-col p-4 align-top">
             <div>
@@ -18,7 +33,7 @@ export default function TextGenTemplate({
                 </div>
             </div>
             
-            <span className="font-semibold text-lg pt-4 pb-2">{name}</span>
+            <span className="font-semibold text-lg pt-4 pb-2 line-clamp-2">{name}</span>
             <span className="font-regular text-medium opacity-70">{description}</span>
         </div>
     );
